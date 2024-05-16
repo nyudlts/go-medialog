d9e018180a1688f569010a9ebf269bd285e86e7b
Don Mennerich <don.mennerich@nyu.edu>
2024-05-16 16:07:56 -0400

    updating test fixtures

--- testing/entries_test.go
+++ testing/entries_test.go
@@ -11,44 +11,13 @@ import (
 
 func TestEntries(t *testing.T) {
 
-	t.Run("Test Unique Media ID in Resource", func(t *testing.T) {
-		mediaID := 5
-		resourceID := uint(30)
-
-		got, err := database.IsMediaIDUniqueInResource(mediaID, resourceID)
-		if err != nil {
-			t.Error(err)
-		}
-
-		want := true
-		if want != got {
-			t.Errorf("Wanted %v, got %v", want, got)
-		}
-	})
-
-	t.Run("Test Non-Unique Media ID in Resource", func(t *testing.T) {
-
-		mediaID := 1
-		resourceID := uint(30)
-
-		got, err := database.IsMediaIDUniqueInResource(mediaID, resourceID)
-		if err != nil {
-			t.Error(err)
-		}
-
-		want := false
-		if want != got {
-			t.Errorf("Wanted %v, got %v", want, got)
-		}
-
-	})
-
 	var entryID uuid.UUID
 	t.Run("Test create an entry", func(t *testing.T) {
 		uid, _ := uuid.NewUUID()
 		entry := models.Entry{}
 		entry.ID = uid
 		entry.MediaID = 789
+		entry.CollectionID = 1
 		entry.ImagedBy = "Donald Mennerich"
 		var err error
 		entryID, err = database.InsertEntry(&entry)
@@ -73,6 +42,38 @@ func TestEntries(t *testing.T) {
 		}
 
 		t.Logf("got accession: %s", string(b))
+
+	})
+
+	t.Run("Test Unique Media ID in Resource", func(t *testing.T) {
+		mediaID := 5
+		resourceID := uint(1)
+
+		got, err := database.IsMediaIDUniqueInResource(mediaID, resourceID)
+		if err != nil {
+			t.Error(err)
+		}
+
+		want := true
+		if want != got {
+			t.Errorf("Wanted %v, got %v", want, got)
+		}
+	})
+
+	t.Run("Test Non-Unique Media ID in Resource", func(t *testing.T) {
+
+		mediaID := 789
+		resourceID := uint(1)
+
+		got, err := database.IsMediaIDUniqueInResource(mediaID, resourceID)
+		if err != nil {
+			t.Error(err)
+		}
+
+		want := false
+		if want != got {
+			t.Errorf("Wanted %v, got %v", want, got)
+		}
 
 	})
 
--- testing/shared.go
+++ testing/shared.go
@@ -1,16 +1,51 @@
 package test
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/nyudlts/go-medialog/database"
+	"gopkg.in/yaml.v2"
 )
 
 var dbLocat string
+var config string
+
+type Environment struct {
+	LogLocation     string `yaml:"log"`
+	DatbaseLocation string `yaml:"database"`
+}
 
 func init() {
 
-	dbLocat = "../database/medialog-test.db"
-
-	if err := database.ConnectDatabase(dbLocat); err != nil {
+	bytes, err := os.ReadFile("../config/go-medialog.conf")
+	if err != nil {
 		panic(err)
 	}
+
+	env, err := getEnvironment("test", bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := database.ConnectDatabase(env.DatbaseLocation); err != nil {
+		panic(err)
+	}
+}
+
+func getEnvironment(environment string, configBytes []byte) (Environment, error) {
+	envMap := map[string]Environment{}
+
+	err := yaml.Unmarshal(configBytes, &envMap)
+	if err != nil {
+		return Environment{}, err
+	}
+
+	for k, v := range envMap {
+		if environment == k {
+			return v, nil
+		}
+	}
+
+	return Environment{}, fmt.Errorf("Error")
 }
