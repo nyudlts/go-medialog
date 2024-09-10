3ef4aa6e0b33644ff5476a7e6192cb94031dcd50
Don Mennerich <don.mennerich@nyu.edu>
2024-09-10 17:27:58 -0400

    updating accessions-show view to generate csv file.

--- medialog_test.go
+++ medialog_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -22,6 +24,41 @@ func TestApplication(t *testing.T) {
 		r.ServeHTTP(recorder, req)
 		assert.Equal(t, 401, recorder.Code)
 		assert.Equal(t, "text/html; charset=utf-8", recorder.Header().Get("content-type"))
+	})
+
+	t.Run("test login to application", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(recorder)
+		form := url.Values{}
+		form.Set("email", env.TestCreds.Username)
+		form.Add("password_1", env.TestCreds.Password)
+		req, err := http.NewRequestWithContext(c, "POST", "/users/authenticate", strings.NewReader(form.Encode()))
+		if err != nil {
+			t.Error(err)
+		}
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		r.ServeHTTP(recorder, req)
+		assert.Equal(t, http.StatusFound, recorder.Code)
+
+		t.Log(recorder.Result().Header.Get("Set-Cookie"))
+	})
+
+	t.Run("test get index authenticated", func(t *testing.T) {
+		recorder := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(recorder)
+		c.SetCookie("medialog-sessions",
+			"MTcyNTk5NzI2MHxOd3dBTkZkRVFUZEhRazFKU1VWVldqVlRSRlpZUkVWSVYxQkRRMUpHTjAxTFdsVk9OME5HTWpWTVdqSkhURnBPUkVsVVNGWmFWVkU9fGYXoOEZhC7V7FyNzW3NZd0xiPTGNjOqHJg2LMMM5iex",
+			2592000, "/", "", false, true,
+		)
+
+		req, err := http.NewRequestWithContext(c, "GET", "/", nil)
+		if err != nil {
+			t.Error(err)
+		}
+		r.ServeHTTP(recorder, req)
+		assert.Equal(t, 200, recorder.Code)
+		assert.Equal(t, "text/html; charset=utf-8", recorder.Header().Get("content-type"))
+
 	})
 
 }
