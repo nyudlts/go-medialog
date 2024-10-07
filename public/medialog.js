26aba81e303249f5a80245627172f591fe35620e
Don Mennerich <don.mennerich@nyu.edu>
2024-10-07 14:13:40 -0400

    adding password toggle to login screen

---
+++ public/medialog.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+	var passwd = document.getElementById("password_1");
+	if (passwd.type === "password") {
+	  passwd.type = "text";
+	} else {
+	  passwd.type = "password";
+	}
+  } 
