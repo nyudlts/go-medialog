44cac95f70482c618753892f6b0b4ed19733ea18
Don Mennerich <don.mennerich@nyu.edu>
2025-03-10 13:12:07 -0400

    updating accesions filter

--- public/filter-accessions.js
+++ public/filter-accessions.js
@@ -1,19 +1,23 @@
 function filterAccessions() {
+	
     var input = document.getElementById("filterText");
     var filter = input.value.toUpperCase();
     var table = document.getElementById("accessionsTable");
     var tr = table.getElementsByTagName("tr");
     
-	// Loop through all table rows, and hide those who don't match the search query
-	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[2];
-		if (td) {
-		txtValue = td.textContent || td.innerText;
-		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-			tr[i].style.display = "";
-		} else {
-			tr[i].style.display = "none";
-		}
+	for (var i = 0; i < tr.length; i++) {
+		var td0 = tr[i].getElementsByTagName("td")[0];
+		var td1 = tr[i].getElementsByTagName("td")[1];
+		var td2 = tr[i].getElementsByTagName("td")[2];
+		if (td0 || td1 || td2) {
+			txtValue1 = td0.textContent || td0.innerText;
+			txtValue2 = td1.textContent || td1.innerText;
+			txtValue3 = td2.textContent || td2.innerText;
+			if ( txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1 || txtValue3.toUpperCase().indexOf(filter) > -1) {
+				tr[i].style.display = "";
+			} else {
+				tr[i].style.display = "none";
+			}
 		}
 	}
 }
