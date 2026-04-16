0fecc054bd41b7933c108dc3c9df0746a173c98a
Donald Mennerich <don.mennerich@nyu.edu>
2026-04-16 13:00:56 -0400

    updating build

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-go build -o medialog ../medialog.go
+Os=""
+Od=""
+Path=""
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -Os) Os="$2"; shift 2 ;;
+        -Od) Od="$2"; shift 2 ;;
+        -Path) Path="$2"; shift 2 ;;
+        *) echo "Unknown argument: $1"; exit 1 ;;
+    esac
+done
+
+GOOS="$Os" go build -o "$Od" "$Path"
